1b52fafc2d4051d46dddd7ea24165d6c01d2e396
Emiri <emiripoint@proton.me>
2023-12-28 03:11:28 -0700

    Update 簡単上書き.py
    メインスクリプトを追加しました。公開PGPキーが添付されています。
    
    -----BEGIN PGP PUBLIC KEY BLOCK-----
    
    mQINBGWNIRQBEAC+UjF7VbgRSN6qzOMD6zuFYRd64qGnW1/N6V1fEdN7otmqNfxA
    Tt8bugPHIkimWsf2G+/rGRdasm8SddZhE+Wv+mgs2E6iLKgeHc4kMqHnQ0Wa17b2
    ojq3Zhw62+Nw3AxK81CYLKwBVjARNv/LDSt4or+8vd6gl9BDUmrtOlMPqq7aAdSk
    P16TzNKHxNPUjvytSJiGoIkb7s2Lpp4HCLyocd0GD+2OYkIEs9H2bPmL7lIC4l7y
    RAwMsnCKJsZV4/TAvJEYdy0aWASyOwthLycbiFfUT+d/R/Wgz99V7qzEZ+GFt1lR
    3AUovfSDNUy7jarWartzj447no6E+IUbse1VAKDbyD6KMyKSxa6unR4nwdTl23sj
    R/DBInSwnTr8r5wnD/ZG+6kRUtkpRrgaAFDeRAf+LiUj3uNN0j61bOPffS9vJt5G
    BAxWKtytW1IrNLD9g/vCIKerVI9KtMEzWYxsPtWUMxOjivnjX06v2Xd7crTlrrEO
    FEQI9iOHAR4fkqACKCnkk4ZTF5U/Bg2LTIF6uNhLyeQVG3CnwOiA9FBsY4rGP674
    iyZFCSQ6cYhyUhd6vCZZyXM8a0P08G2JNpUY9zsg6WUXNwPIVbtGLNzj8Uurt5mO
    0NB2Pc14k1bZI50OBvXnFpNRN+ahvnXFxfCJcQKY/VwfrkvjPzDVLI6qvwARAQAB
    tCJFbWlyaSBQb2ludCA8ZW1pcmlwb2ludEBwcm90b24ubWU+iQJOBBMBCAA4FiEE
    Oas1Gtj5jpWIax195vqYlm/RcSEFAmWNIRQCGwMFCwkIBwIGFQoJCAsCBBYCAwEC
    HgECF4AACgkQ5vqYlm/RcSHv3A/+LWG5KuagS6VM6SL+a/zytAf+MwjBFc6IPJIU
    mZTAVr7SJuEOp9imQsy1qbXQ24kuzzWwV3ZhmqUrcNfK2+YQbOT773OZL2Gx7167
    XP5AsegF2vUU7zsSMaYJKIh5PbLnzOUnE9rdAY93aJCIHWG7m4+Zjn0zAObXyr6t
    5tv78/M4YalLnAI/LrHGyfcGvr37ReRqktRxzZWDrMj3yz0+dQzp0wHeQMJOZo79
    KzYQvH98v7YYHDCw/MxAbteEoID5SGfNtDZ/dYjrc/JLW3kvBvyueEwGThLsbsvP
    Lu/cFMN6de0rUZJ1RABDqLotBDfc8gBs2kzyHvKoJMBSOv+0nzBY2dSx9H1m5HJh
    nlJ/P7OizD4wIDnvMRCp6SKeagqa0iVuDrPQ7k9UGOTKBiS7r2XyjcM1Yto9AeTZ
    H7QAJzk83+wFNBqXr38r3i9Q24YJWgEZzT0RabZAOXt6tc+ubCmsC1NmpzEV4i2i
    LnUqNOOMMZUk2EK/JXQI6R/G7vYLupv9XTLkwxKhT1loulYsi7AOpYrPlSih0SFv
    epNGm0YfGbu0oONnA1QEniNCRII1tJIxWyWaLcXJMw6YntND9ZhGK93DGQZKBxS8
    dwhcUKEpyI4Z7fFZcWn+5NuwiOGLJc36ZZzP9HUhKHgHMPB1tLuJPvq8sKkxydMF
    JU7XTwO5Ag0EZY0hFAEQANnGYALg5cGPlInYU9uHEpuK1pOpNKU9jqaPA6F87d2X
    CAXdo8dM4n3mbvu1YoFOkM6xEhmcllIsqmuN5Lei1eMiB75sqxZtee/ygTEMNyoN
    pzL7QQf16mCieUcNFRAJ0nRJLQmUOg9E/5ZoGEaSs0QWLL9GY/QuviTXgtRy85SW
    OzUoH8lZbSlLW3ud+5BbwNB19I6xeKQP0NyyY2NQFfDdCExJGbTvMPHNguPa6413
    ZKsdoK3MSaWEBdM5/gJgquzXzW5u3x/blTx5xmX+VD+O5su6eu9vicZ9stfQJl9e
    ONbQm0VaBhiVmdl6Puyah8Y7lt5zFb3l9xGFhX9cx/d1fXGki9s30NJv5jl2+kIO
    U2WYrXyliunhjAXYcydZ9jH6Y8rIp+OsLJeHCodtEsEKllir/qwPOhFDRHvPR9fM
    6UA2KTnzT7sTaZxqQ7Bd0xAkt4HZjICCGS86cza8bdVDXcWfwSXG/H4Jz1UHsrw7
    QLOKCgp9IH4xHzg9pApv2EeGFMDB+gG/0ACYgHWAID1KBmW6Gm/fvFZDjhmIZXsq
    8Krqv7XBtk+yYZTH8eBpEqmoDakiL3KaVsuPGe3SGSylq763/TX15F9Brs3iOUFu
    aCXN1AYfoo9fn10/UysuemVlhG5ubnfnmB51GSOoJy2IQ3rU5+4vbdbzZze/O4sB
    ABEBAAGJAjYEGAEIACAWIQQ5qzUa2PmOlYhrHX3m+piWb9FxIQUCZY0hFAIbDAAK
    CRDm+piWb9FxIU7jEACnwgwAwSMBSt6O+IxIoFYbdgvyHlnsltlDzYmD99Wm5wAw
    Pt3ogQU2r+TMiGPVDcG6hve6n4kK89uZalM7IRvS2HDgejocb70CY6ojyXqRPFZh
    II9zaHv+HIHwqE3hEHR/EUeLvMfOO6AdtN7KM8iZJsEfeEq7qUMnr1l1YUSDuhSg
    jB3vOIxdAYWWeNCPX8d5gcfslCu5arii7VLR1plgLWVp32MaFc0maHFumDeCcrlq
    b7iwG26K8qKYvgE6Lw0GdVfAOd67I18xLl5RWVuCf8ym7DEpKA4fUHuZGXPTg+J2
    jIrCCrHpK1P5WNKwAbhVMx4EfmZF37rzV1HBtD29RO7kbSRE+9wEAOt5DICEmRvf
    Q6TkUtCZKdT7KTC85vGW6M2ljWYGQqJ9Bc3PbOK/sY5govNfuHqmtTDGrVML4ds4
    otAANVdbA/2dHrTCP+0KZ7bM6YK2Ccgbhl+tNfX+IolwJW106fIQjUUmh0V0xo+3
    Xd8em232HXu14NQQs3BHbAvYc+/e+19WWkuvZsYTvruStXJrUxzUQadQzPwRdFDz
    z12ANSWYtEGdVoAGqxImTwv5JhSY0DEivFgIU98N7Xgwtr0TWeCnl+u3goR7TbGa
    q5/rPFzPuUWWzRDrWqCZy/RKyDxG1ivDI5zknJnsnYe4gOyV9xSDsqhi16L2rw==
    =TnZV
    -----END PGP PUBLIC KEY BLOCK-----
    
    
    Signed-off-by: Emiri <emiripoint@proton.me>

--- 簡単上書き.py
+++ 簡単上書き.py
@@ -1,1 +1,38 @@
+import os
+import argparse
+from moviepy.editor import VideoFileClip, ImageClip, CompositeVideoClip
 
+def process_videos(video_folder, overlay_image_path):
+    # フォルダ内のすべてのファイルを反復処理します
+    for filename in os.listdir(video_folder):
+        if filename.endswith(".mp4"):
+            video_path = os.path.join(video_folder, filename)
+            
+            # 動画とオーバーレイ画像を読み込む
+            video = VideoFileClip(video_path)
+            overlay = ImageClip(overlay_image_path).set_duration(video.duration).resize(video.size)
+
+            # 動画の上に画像をオーバーレイする
+            final_video = CompositeVideoClip([video, overlay], size=video.size)
+            
+            # 出力ファイルパスを定義する
+            output_path = os.path.join(video_folder, 'プロセス済み_' + filename)
+            
+            # ファイルに結果を書き込む
+            final_video.write_videofile(output_path, codec="libx264", audio_codec="aac")
+
+            print(f"Processed {filename}")
+
+def main():
+    parser = argparse.ArgumentParser(description="MP4動画の一連に画像をオーバーレイします。")
+    parser.add_argument("video_folder",
+                        help="MP4動画が含まれているディレクトリへのパス。")
+    parser.add_argument("overlay_image",
+                        help="オーバーレイ画像へのパス。")
+    
+    args = parser.parse_args()
+
+    process_videos(args.video_folder, args.overlay_image)
+
+if __name__ == "__main__":
+    main()
